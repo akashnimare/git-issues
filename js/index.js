3aa61f49558941945b2b4c6f2167c0fdcb151cca
Akash Nimare <svnitakash@gmail.com>
2018-07-03 04:22:32 +0530

    Update routing.

--- js/index.js
+++ js/index.js
@@ -65,6 +65,35 @@ app.controller("repoCtrl", [
         $scope.comments = data;
       });
     };
+    $scope.getGitInfo = function() {
+    $scope.userNotFound = false;
+    $scope.loaded = false;
+    $scope.nouser = false;
+    $http
+      .get(
+        "https://api.github.com/repos/" +
+          $scope.username +
+          "/issues?state=open&page=" +
+          $scope.main.page +
+          "&per_page=20"
+      )
+      .success(function(data) {
+        // This data contains both pull_requests and issues since we only
+        // need the issues let's filter out the pull_requests
+        let getAllIssues = data.filter(Element => {
+          return !Element["pull_request"];
+        });
+        $scope.user = getAllIssues;
+        $scope.loaded = true;
+      })
+      .error(function(err) {
+        $scope.userNotFound = true;
+        if (!$scope.username) {
+          $scope.errorName = "Please enter a vaild repo name";
+        } else {
+          $scope.errorName = "No open issue found for " + $scope.username;
+        }
+      })};
     // };
     $scope.nextPage = function() {
       $scope.main.page++;
