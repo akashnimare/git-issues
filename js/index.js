78644cffb7c73adaa997dc3c9a5e102032cd213f
Akash Nimare <svnitakash@gmail.com>
2018-07-02 11:42:12 +0530

    Fix styling issue.

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ app.controller("gitCtrl", function($scope, $http, marked) {
           $scope.username +
           "/issues?state=open&page=" +
           $scope.main.page +
-          "&per_page=30"
+          "&per_page=20"
       )
       .success(function(data) {
         // This data contains both pull_requests and issues since we only
