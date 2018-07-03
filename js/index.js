7707aa76bdac501cfa5a62433b9f1d672f9db910
Akash Nimare <svnitakash@gmail.com>
2018-07-03 20:13:41 +0530

    Update views.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ app.config([
   function($stateProvider, $urlRouterProvider) {
     $stateProvider.state("home", {
       url: "/home",
-      templateUrl: "/home.html",
+      templateUrl: "/repo.html",
       controller: "mainCtrl"
     });
 
