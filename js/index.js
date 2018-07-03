9e004db474a08285dc4f771f563bf6bd8219e4d2
Akash Nimare <svnitakash@gmail.com>
2018-07-03 16:50:44 +0530

    Add main controller.

--- js/index.js
+++ js/index.js
@@ -20,19 +20,27 @@ app.config([
   }
 ]);
 
+app.controller("mainCtrl", [
+  "$scope",
+  "$http",
+  function($scope, $http) {
+    $scope.main = {
+      page: 1
+    };
+    $scope.UserComment = function(event) {
+      $http.get(event.target.id).success(function(data) {
+        $scope.comments = data;
+      });
+    };
+  }
+]);
+
 app.controller("repoCtrl", [
   "$scope",
   "$stateParams",
   "$http",
   function($scope, $stateParams, $http, marked) {
-    $scope.msg = $stateParams.username + " " + $stateParams.reponame;
-    console.log($stateParams.username);
-    console.log($stateParams.reponame);
-    $scope.oneAtATime = true;
-    $scope.main = {
-      page: 1
-    };
-    $scope.username = $stateParams.username + '/' + $stateParams.reponame;
+    $scope.username = $stateParams.username + "/" + $stateParams.reponame;
     console.log($scope.username);
     $scope.loaded = false;
     $http
@@ -62,41 +70,37 @@ app.controller("repoCtrl", [
           $scope.errorName = "No open issue found for " + $scope.username;
         }
       });
-    $scope.UserComment = function(event) {
-      $http.get(event.target.id).success(function(data) {
-        $scope.comments = data;
-      });
-    };
+
     $scope.getGitInfo = function() {
-    $scope.userNotFound = false;
-    $scope.loaded = false;
-    $scope.nouser = false;
-    $http
-      .get(
-        "https://api.github.com/repos/" +
-          $scope.username +
-          "/issues?state=open&page=" +
-          $scope.main.page +
-          "&per_page=20"
-      )
-      .success(function(data) {
-        // This data contains both pull_requests and issues since we only
-        // need the issues let's filter out the pull_requests
-        let getAllIssues = data.filter(Element => {
-          return !Element["pull_request"];
+      $scope.userNotFound = false;
+      $scope.loaded = false;
+      $scope.nouser = false;
+      $http
+        .get(
+          "https://api.github.com/repos/" +
+            $scope.username +
+            "/issues?state=open&page=" +
+            $scope.main.page +
+            "&per_page=20"
+        )
+        .success(function(data) {
+          // This data contains both pull_requests and issues since we only
+          // need the issues let's filter out the pull_requests
+          let getAllIssues = data.filter(Element => {
+            return !Element["pull_request"];
+          });
+          $scope.user = getAllIssues;
+          $scope.loaded = true;
+        })
+        .error(function(err) {
+          $scope.userNotFound = true;
+          if (!$scope.username) {
+            $scope.errorName = "Please enter a vaild repo name";
+          } else {
+            $scope.errorName = "No open issue found for " + $scope.username;
+          }
         });
-        $scope.user = getAllIssues;
-        $scope.loaded = true;
-      })
-      .error(function(err) {
-        $scope.userNotFound = true;
-        if (!$scope.username) {
-          $scope.errorName = "Please enter a vaild repo name";
-        } else {
-          $scope.errorName = "No open issue found for " + $scope.username;
-        }
-      })};
-    // };
+    };
     $scope.nextPage = function() {
       $scope.main.page++;
       $scope.getGitInfo();
@@ -109,10 +113,6 @@ app.controller("repoCtrl", [
 ]);
 
 app.controller("gitCtrl", function($scope, $http, marked) {
-  $scope.oneAtATime = true;
-  $scope.main = {
-    page: 1
-  };
   $scope.loaded = false;
   $scope.errorName = "";
   $scope.getGitInfo = function() {
@@ -144,18 +144,13 @@ app.controller("gitCtrl", function($scope, $http, marked) {
           $scope.errorName = "No open issue found for " + $scope.username;
         }
       });
-    $scope.UserComment = function(event) {
-      $http.get(event.target.id).success(function(data) {
-        $scope.comments = data;
-      });
+    $scope.nextPage = function() {
+      $scope.main.page++;
+      $scope.getGitInfo();
     };
-  };
-  $scope.nextPage = function() {
-    $scope.main.page++;
-    $scope.getGitInfo();
-  };
-  $scope.prePage = function() {
-    $scope.main.page--;
-    $scope.getGitInfo();
+    $scope.prePage = function() {
+      $scope.main.page--;
+      $scope.getGitInfo();
+    };
   };
 });
