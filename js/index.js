fad8726267770309ab09e0b99d5423a394ba7b35
Akash Nimare <svnitakash@gmail.com>
2018-07-03 18:05:55 +0530

    Refactor code.

--- js/index.js
+++ js/index.js
@@ -7,13 +7,13 @@ app.config([
     $stateProvider.state("home", {
       url: "/home",
       templateUrl: "/home.html",
-      controller: "gitCtrl"
+      controller: "mainCtrl"
     });
 
     $stateProvider.state("repo", {
       url: "/:username/:reponame",
       templateUrl: "/repo.html",
-      controller: "repoCtrl"
+      controller: "mainCtrl"
     });
 
     $urlRouterProvider.otherwise("home");
@@ -23,58 +23,26 @@ app.config([
 app.controller("mainCtrl", [
   "$scope",
   "$http",
-  function($scope, $http) {
+  "$stateParams",
+  "$location",
+  function($scope, $http, $stateParams, $location) {
     $scope.main = {
       page: 1
     };
-    $scope.UserComment = function(event) {
-      $http.get(event.target.id).success(function(data) {
-        $scope.comments = data;
-      });
-    };
-  }
-]);
-
-app.controller("repoCtrl", [
-  "$scope",
-  "$stateParams",
-  "$http",
-  function($scope, $stateParams, $http, marked) {
-    $scope.username = $stateParams.username + "/" + $stateParams.reponame;
-    console.log($scope.username);
-    $scope.loaded = false;
-    $http
-      .get(
-        "https://api.github.com/repos/" +
-          $stateParams.username +
-          "/" +
-          $stateParams.reponame +
-          "/issues?state=open&page=" +
-          $scope.main.page +
-          "&per_page=20"
-      )
-      .success(function(data) {
-        // This data contains both pull_requests and issues since we only
-        // need the issues let's filter out the pull_requests
-        let getAllIssues = data.filter(Element => {
-          return !Element["pull_request"];
-        });
-        $scope.user = getAllIssues;
-        $scope.loaded = true;
-      })
-      .error(function(err) {
-        $scope.userNotFound = true;
-        if (!$scope.username) {
-          $scope.errorName = "Please enter a vaild repo name";
-        } else {
-          $scope.errorName = "No open issue found for " + $scope.username;
-        }
-      });
-
+    // // update hash location
+    // if (!$stateParams.username) {
+    //   $location.url($scope.username);
+    //   console.log($stateParams);
+    // }
     $scope.getGitInfo = function() {
       $scope.userNotFound = false;
       $scope.loaded = false;
       $scope.nouser = false;
+      // $location.url($scope.username);
+      // $scope.username = $stateParams.username
+      //   ? $stateParams.username + "/" + $stateParams.reponame
+      //   : $scope.username;
+
       $http
         .get(
           "https://api.github.com/repos/" +
@@ -101,6 +69,9 @@ app.controller("repoCtrl", [
           }
         });
     };
+    if ($stateParams.username) {
+      $scope.getGitInfo();
+    }
     $scope.nextPage = function() {
       $scope.main.page++;
       $scope.getGitInfo();
@@ -108,49 +79,11 @@ app.controller("repoCtrl", [
     $scope.prePage = function() {
       $scope.main.page--;
       $scope.getGitInfo();
+    };
+    $scope.UserComment = function(event) {
+      $http.get(event.target.id).success(function(data) {
+        $scope.comments = data;
+      });
     };
   }
 ]);
-
-app.controller("gitCtrl", function($scope, $http, marked) {
-  $scope.loaded = false;
-  $scope.errorName = "";
-  $scope.getGitInfo = function() {
-    $scope.userNotFound = false;
-    $scope.loaded = false;
-    $scope.nouser = false;
-    $http
-      .get(
-        "https://api.github.com/repos/" +
-          $scope.username +
-          "/issues?state=open&page=" +
-          $scope.main.page +
-          "&per_page=20"
-      )
-      .success(function(data) {
-        // This data contains both pull_requests and issues since we only
-        // need the issues let's filter out the pull_requests
-        let getAllIssues = data.filter(Element => {
-          return !Element["pull_request"];
-        });
-        $scope.user = getAllIssues;
-        $scope.loaded = true;
-      })
-      .error(function(err) {
-        $scope.userNotFound = true;
-        if (!$scope.username) {
-          $scope.errorName = "Please enter a vaild repo name";
-        } else {
-          $scope.errorName = "No open issue found for " + $scope.username;
-        }
-      });
-    $scope.nextPage = function() {
-      $scope.main.page++;
-      $scope.getGitInfo();
-    };
-    $scope.prePage = function() {
-      $scope.main.page--;
-      $scope.getGitInfo();
-    };
-  };
-});
